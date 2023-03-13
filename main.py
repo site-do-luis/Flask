d4d463b05bf56e93ff50e88e3da66dd54b41fa88
Luis Gabriel <68129466+site-do-luis@users.noreply.github.com>
2023-03-13 18:58:16 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 class Jogo():
@@ -7,24 +7,34 @@ class Jogo():
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('League of Legends','Moba','PC')
+jogo2 = Jogo('Counter Strike: Global Offensive','Tiro','PC')
+jogo3 = Jogo('Call os Duty','Tiro','PS2')
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
-@app.route('/inicio')
+@app.route('/')
 
-def ola():
-    titulo = 'Jogos'
-    jogo1 = Jogo('League of Legends','Moba','PC')
-    jogo2 = Jogo('Counter Strike: Global Offensive','Tiro','PC')
-    jogo3 = Jogo('Call os Duty','Tiro','PS2')
-    jogo4 = Jogo('Need for Speed','Corrida','PS2')
-    lista = [jogo1, jogo2, jogo3, jogo4]
-    return render_template('lista.html', titulo = titulo, jogos=lista)
+def index():
+    return render_template('lista.html', titulo = 'Jogos', jogos=lista)
+
+
+@app.route('/criar', methods=['POST',])
+
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    Jogo_new = Jogo(nome, categoria, console)
+    lista.append(Jogo_new)
+    return redirect('/')
+    # return render_template('lista.html', titulo = 'Jogos', jogos=lista)
 
 
 @app.route('/novo')
 
 def novo():
-    titulo = 'Novo Jogo'
-    return render_template('novo.html', titulo = titulo)
+    return render_template('novo.html', titulo = 'Novo Jogo')
 
 app.run(debug=True)
